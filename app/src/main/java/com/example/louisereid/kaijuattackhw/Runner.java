ec992c864cd4fc4d418497aaa14f6d9c0dc3290a
LouiseReid <l_reid@hotmail.co.uk>
2017-10-25 20:23:36 +0100

    runner

--- app/src/main/java/com/example/louisereid/kaijuattackhw/Runner.java
+++ app/src/main/java/com/example/louisereid/kaijuattackhw/Runner.java
@@ -7,5 +7,21 @@ package com.example.louisereid.kaijuattackhw;
 public class Runner {
 
     Tank tank;
+    Chopper chopper;
+    KingKong kingKong;
+    Godzilla godzilla;
+
+    public Runner(Tank tank, Chopper chopper, KingKong kingKong, Godzilla godzilla) {
+        this.tank = tank;
+        this.chopper = chopper;
+        this.kingKong = kingKong;
+        this.godzilla = godzilla;
+
+
+
+        System.out.println(kingKong.attack());
+        System.out.println(tank.hitByKingKong());
+    }
+
 
 }
